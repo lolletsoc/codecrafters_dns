d61a29957ce86452f3a6132ad2bcc0e980a7c0f3
Liam Costello <liam@dehook.io>
2024-08-24 19:01:37 +0100

    deku wins

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,98 @@
-// Uncomment this block to pass the first stage
 use std::net::UdpSocket;
-use nom::Slice;
-use packed_struct::prelude::{packed_bits, PackedStruct};
-use packed_struct::types::Integer;
+use deku::prelude::*;
+use nom::AsBytes;
 
-#[derive(PackedStruct, Debug)]
-#[packed_struct(bit_numbering = "msb0")]
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(endian = "big")]
 struct Header {
-    #[packed_field(bits = "0..", endian = "msb")]
+    #[deku(bits = 16)]
     id: u16,
 
-    #[packed_field(endian = "msb")]
-    qr: Integer<u8, packed_bits::Bits<1>>,
+    #[deku(bits = 1)]
+    qr: u8,
 
-    #[packed_field(endian = "msb")]
-    opcode: Integer<u8, packed_bits::Bits<4>>,
+    #[deku(bits = 4)]
+    opcode: u8,
 
-    #[packed_field(endian = "msb")]
-    aa: Integer<u8, packed_bits::Bits<1>>,
+    #[deku(bits = 1)]
+    aa: u8,
 
-    #[packed_field(endian = "msb")]
-    tc: Integer<u8, packed_bits::Bits<1>>,
+    #[deku(bits = 1)]
+    tc: u8,
 
-    #[packed_field(endian = "msb")]
-    rd: Integer<u8, packed_bits::Bits<1>>,
+    #[deku(bits = 1)]
+    rd: u8,
 
-    #[packed_field(endian = "msb")]
-    ra: Integer<u8, packed_bits::Bits<1>>,
+    #[deku(bits = 1)]
+    ra: u8,
 
     // Reserved
-    #[packed_field(endian = "msb")]
-    z: Integer<u8, packed_bits::Bits<3>>,
+    #[deku(bits = 3)]
+    z: u8,
 
-    #[packed_field(endian = "msb")]
-    rcode: Integer<u8, packed_bits::Bits<4>>,
+    #[deku(bits = 4)]
+    rcode: u8,
 
-    #[packed_field(endian = "msb")]
+    #[deku(bits = 16)]
     qdcount: u16,
 
-    #[packed_field(endian = "msb")]
+    #[deku(bits = 16)]
     ancount: u16,
 
-    #[packed_field(endian = "msb")]
+    #[deku(bits = 16)]
     nscount: u16,
 
-    #[packed_field(endian = "msb")]
+    #[deku(bits = 16)]
     arcount: u16,
 
 }
 
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(endian = "big")]
 struct Question {
-    name: String,
+    #[deku(until = "|v: &u8| *v == b'\0'")]
+    name: Vec<u8>,
+
+    #[deku(bits = 16)]
     _type: u16,
+
+    #[deku(bits = 16)]
     class: u16,
 }
 
-#[derive(PackedStruct, Debug)]
-#[packed_struct(bit_numbering = "msb0")]
-struct Message {
-    #[packed_field(bytes = "0..13", endian = "msb")]
-    header: Header,
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(endian = "big")]
+struct Answer {
+    #[deku(until = "|v: &u8| *v == b'\0'")]
+    name: Vec<u8>,
 
-    #[packed_field(endian = "msb")]
-    question: [u8; 500],
+    #[deku(bits = 16)]
+    _type: u16,
+
+    #[deku(bits = 16)]
+    class: u16,
+
+    #[deku(bits = 32)]
+    ttl: u32,
+
+    #[deku(update = "self.data.len()")]
+    length: u16,
+
+    #[deku(count = "length")]
+    data: Vec<u8>,
+
 }
 
-fn construct_questions_from(bytes: &[u8]) -> String {
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+struct Message {
+    header: Header,
+
+    question: Question,
+
+    answer: Answer,
+}
+
+fn construct_lookup(bytes: &[u8]) -> (String, u8) {
     let mut lookup_parts = vec![];
     let mut i: u8 = 0;
     loop {
@@ -74,16 +101,15 @@ fn construct_questions_from(bytes: &[u8]) -> String {
             break;
         }
 
-        lookup_parts.push(String::from_utf8_lossy(&bytes[(i + 1) as usize..(length + 1) as usize]).to_string());
+        lookup_parts.push(String::from_utf8_lossy(&bytes[(i + 1) as usize..(i + length + 1) as usize]).to_string());
 
-        i += length;
+        i += length + 1;
     }
 
-    lookup_parts.join(".")
+    (lookup_parts.join("."), i)
 }
 
 fn main() {
-    // Uncomment this block to pass the first stage
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
 
     // DNS is limited to 512 bytes
@@ -92,16 +118,26 @@ fn main() {
     loop {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
-                println!("Received {} bytes from {}", size, source);
+                match Message::from_bytes((&buf, 0)) {
+                    Ok(result) => {
+                        let mut message = result.1;
+                        let answer = Answer {
+                            name: message.question.name.to_owned(),
+                            _type: 1,
+                            class: 1,
+                            ttl: 60,
+                            length: 4,
+                            data: vec!(0x08, 0x08, 0x08, 0x08),
+                        };
 
-                match Message::unpack(&buf) {
-                    Ok(mut message) => {
-                        message.header.qr = 1.into();
-                        let lookup = construct_questions_from(&message.question);
-                        println!("Received request for {}", lookup);
+                        message.header.qr = 1;
+                        message.header.ancount = 1;
+                        message.answer = answer;
+                        let lookup = construct_lookup(message.question.name.as_bytes());
+                        println!("Looking up '{}'", lookup.0);
 
                         udp_socket
-                            .send_to(&message.pack().unwrap(), source)
+                            .send_to(message.to_bytes().unwrap().as_bytes(), source)
                             .expect("Failed to send response");
                     }
                     Err(_) => {
