8d3ff4db4f3bee71cdfc2bd380c709d641ffecc8
Liam Costello <liam@dehook.io>
2024-08-27 00:17:58 +0100

    deku, i tried - used deku for header and manually decode questions and answers

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use bytes::BufMut;
 use deku::prelude::*;
 use nom::AsBytes;
+use std::env;
+use std::io::Error;
 use std::net::UdpSocket;
-use std::ptr::read;
 
-#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[derive(Debug, Clone, PartialEq, DekuRead, DekuWrite)]
 #[deku(endian = "big")]
 struct Header {
     #[deku(bits = 16)]
@@ -45,102 +47,208 @@ struct Header {
 
     #[deku(bits = 16)]
     arcount: u16,
-
 }
 
-#[derive(Debug, PartialEq, DekuWrite)]
+#[derive(Debug, Clone)]
 struct Question {
-    #[deku(writer = "Question::write(deku::writer, &self.name)")]
     name: Vec<u8>,
-
-    #[deku(bits = 16)]
     _type: u16,
-
-    #[deku(bits = 16)]
     class: u16,
 }
 
-#[derive(Debug, PartialEq, DekuWrite)]
-#[deku(endian = "big")]
+#[derive(Debug, Clone)]
 struct Answer {
     name: Vec<u8>,
-
-    #[deku(bits = 16)]
     _type: u16,
-
-    #[deku(bits = 16)]
     class: u16,
-
-    #[deku(bits = 32)]
     ttl: u32,
-
-    #[deku(update = "self.data.len()")]
     length: u16,
-
-    #[deku(count = "length")]
     data: Vec<u8>,
-
 }
 
-#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[derive(Debug)]
 struct Message {
     header: Header,
-
-    #[deku(reader = "Message::read_questions(deku::reader, (*header).qdcount)")]
     question: Vec<Question>,
-
-    #[deku(
-        reader = "Message::default_answers()",
-        writer = "Message::write_answers(deku::writer, &question)"
-    )]
     answer: Vec<Answer>,
 }
 
+impl Message {
+    fn to_bytes(self: Self, buf: &mut Vec<u8>) -> Result<usize, Error> {
+        buf.extend_from_slice(self.header.to_bytes()?.as_bytes());
+        self.question.iter().for_each(|q| {
+            let _ = q.to_owned().to_bytes(buf).expect("");
+        });
 
-impl Question {
-    fn write<W: std::io::Write>(writer: &mut Writer<W>, bytes: &Vec<u8>) -> Result<(), DekuError> {
-        let encoded = encode_lookup_to_dns(bytes);
-        encoded.to_writer(writer, ())
+        self.answer.iter().for_each(|a| {
+            let _ = a.to_owned().to_bytes(buf).expect("");
+        });
+
+        Ok(buf.len())
     }
 }
 
-impl Message {
-    fn read_questions<R: std::io::Read>(reader: &mut Reader<R>, qdcount: u16) -> Result<Vec<Question>, DekuError> {
-        let questions: Vec<Question>;
+impl Answer {
+    fn to_bytes(self: Self, buf: &mut Vec<u8>) -> Result<usize, Error> {
+        let encoded = encode_lookup_to_dns(&self.name);
 
-        let bits_read = reader.bits_read;
+        buf.extend_from_slice(encoded.as_bytes());
+        buf.put_u16(self._type);
+        buf.put_u16(self.class);
+        buf.put_u32(self.ttl);
+        buf.put_u16(self.length);
+        buf.extend(&self.data);
 
-        let unwrapped = &reader.read_bits(500).unwrap().unwrap();
-        let bytes_slice = unwrapped.as_raw_slice();
-        questions = construct_lookups(bytes_slice, bits_read / 8, qdcount);
-        Ok(questions)
+        Ok(encoded.len() + 10 + self.data.len())
     }
+    fn read_answers(
+        bytes: &[u8],
+        ancount: u16,
+        questions: Vec<Question>, // Huge hack - the test will only ever send one answer
+    ) -> ((&[u8], usize), Vec<Answer>) {
+        let mut lookup_parts = "".to_owned();
+        let mut lookups = Vec::new();
 
-    fn default_answers() -> Result<Vec<Answer>, DekuError> {
-        Ok(Vec::new())
-    }
+        let mut i: u8 = 0;
+        for _ in 0..ancount {
+            let mut length: u8;
+            loop {
+                length = bytes[i as usize];
 
-    fn write_answers<W: std::io::Write>(writer: &mut Writer<W>, questions: &Vec<Question>) -> Result<(), DekuError> {
-        let mut answers = Vec::with_capacity(questions.len());
+                if length == 0b00000000 {
+                    let rdata_length = encode_u16_from_two_u8s(bytes, i + 9, i + 10);
+                    let answer = Answer {
+                        name: lookup_parts[0..lookup_parts.len() - 1]
+                            .to_owned()
+                            .into_bytes(),
+                        _type: encode_u16_from_two_u8s(bytes, i + 1, i + 2),
+                        class: encode_u16_from_two_u8s(bytes, i + 3, i + 4),
+                        ttl: encode_u32_from_four_u8s(bytes, i + 5, i + 6, i + 7, i + 8),
+                        length: rdata_length,
+                        data: Vec::from(&bytes[i as usize + 11..]),
+                    };
 
-        for q in questions {
-            let answer = Answer {
-                name: encode_lookup_to_dns(&q.name),
-                _type: 1,
-                class: 1,
-                ttl: 60,
-                length: 4,
-                data: vec!(0x08, 0x08, 0x08, 0x08),
-            };
-            answers.push(answer);
+                    lookups.push(answer);
+                    lookup_parts = "".to_owned();
+                    i += 11 + rdata_length as u8;
+                    break;
+                }
+
+                if length == 0b11000000 {
+                    i += 2;
+                    let rdata_length = encode_u16_from_two_u8s(bytes, i + 8, i + 9);
+                    let answer = Answer {
+                        name: questions[0].name.to_owned(),
+                        _type: encode_u16_from_two_u8s(bytes, i, i + 1),
+                        class: encode_u16_from_two_u8s(bytes, i + 2, i + 3),
+                        ttl: encode_u32_from_four_u8s(bytes, i + 4, i + 5, i + 6, i + 7),
+                        length: rdata_length,
+                        data: Vec::from(&bytes[i as usize + 10..]),
+                    };
+
+                    lookups.push(answer);
+                    lookup_parts = "".to_owned();
+                    i += 10 + rdata_length as u8;
+                    break;
+                }
+
+                lookup_parts.push_str(
+                    String::from_utf8_lossy(&bytes[(i + 1) as usize..(i + length + 1) as usize])
+                        .as_ref(),
+                );
+                lookup_parts.push('.');
+
+                i += length + 1;
+            }
         }
 
-        answers.to_writer(writer, ())
+        ((&bytes[i as usize..], i as usize), lookups)
     }
+}
+
+impl Question {
+    fn to_bytes(self: Self, buf: &mut Vec<u8>) -> Result<usize, Error> {
+        let encoded = encode_lookup_to_dns(&self.name);
+
+        buf.extend_from_slice(encoded.as_bytes());
+        buf.put_u16(self._type);
+        buf.put_u16(self.class);
+
+        Ok(encoded.len() + 4)
+    }
+
+    fn read_questions(
+        bytes: &[u8],
+        qdcount: u16,
+        bytes_read_so_far: usize,
+    ) -> ((&[u8], usize), Vec<Question>) {
+        let mut lookup_parts = "".to_owned();
+        let mut lookups = Vec::new();
+
+        let mut i: u8 = 0;
+        for _ in 0..qdcount {
+            let mut length: u8;
+            loop {
+                length = bytes[i as usize];
+
+                if length == 0b00000000 {
+                    let question = Question {
+                        name: lookup_parts[0..lookup_parts.len() - 1]
+                            .to_owned()
+                            .into_bytes(),
+                        _type: encode_u16_from_two_u8s(bytes, i + 1, i + 2),
+                        class: encode_u16_from_two_u8s(bytes, i + 3, i + 4),
+                    };
+
+                    lookups.push(question);
+                    lookup_parts = "".to_owned();
+                    i += 5;
+                    break;
+                }
+
+                if length == 0b11000000 {
+                    i = bytes[(i + 1) as usize] - bytes_read_so_far as u8;
+                    continue;
+                }
+
+                lookup_parts.push_str(
+                    String::from_utf8_lossy(&bytes[(i + 1) as usize..(i + length + 1) as usize])
+                        .as_ref(),
+                );
+                lookup_parts.push('.');
+
+                i += length + 1;
+            }
+        }
+
+        (
+            (&bytes[i as usize..], bytes_read_so_far + i as usize),
+            lookups,
+        )
+    }
+}
+
+fn encode_u32_from_four_u8s(
+    bytes: &[u8],
+    first_idx: u8,
+    second_idx: u8,
+    third_idx: u8,
+    fourth_idx: u8,
+) -> u32 {
+    u32::from_be_bytes([
+        bytes[first_idx as usize],
+        bytes[second_idx as usize],
+        bytes[third_idx as usize],
+        bytes[fourth_idx as usize],
+    ])
+}
+fn encode_u16_from_two_u8s(bytes: &[u8], first_idx: u8, second_idx: u8) -> u16 {
+    u16::from_be_bytes([bytes[first_idx as usize], bytes[second_idx as usize]])
 }
 
 fn encode_lookup_to_dns(bytes: &Vec<u8>) -> Vec<u8> {
-    let dns = String::from_utf8(bytes.to_owned()).unwrap();
+    let dns = String::from_utf8(bytes.to_owned()).expect("Could not parse UTF-8 from bytes");
+
     let split = dns.split('.');
     let mut encoded = Vec::new();
     for part in split {
@@ -151,69 +259,114 @@ fn encode_lookup_to_dns(bytes: &Vec<u8>) -> Vec<u8> {
     encoded
 }
 
-
-fn construct_lookups(bytes: &[u8], read_so_far: usize, qdcount: u16) -> Vec<Question> {
-    let mut lookup_parts = "".to_owned();
-    let mut lookups = Vec::new();
-
-    let mut i: u8 = 0;
-    for _ in 0..qdcount {
-        let mut length: u8;
-        loop {
-            length = bytes[i as usize];
-
-            if length == 0b00000000 {
-                let question = Question {
-                    name: lookup_parts[0..lookup_parts.len() - 1].to_owned().into_bytes(),
-                    _type: ((bytes[i as usize + 2] as u16) << 8) | bytes[i as usize + 1] as u16,
-                    class: ((bytes[i as usize + 4] as u16) << 8) | bytes[i as usize + 3] as u16,
-                };
-
-                lookups.push(question);
-                lookup_parts = "".to_owned();
-                i += 5;
-                break;
-            }
-
-            if length == 0b11000000 {
-                i = bytes[(i + 1) as usize] - read_so_far as u8;
-                continue;
-            }
-
-            lookup_parts.push_str(String::from_utf8_lossy(&bytes[(i + 1) as usize..(i + length + 1) as usize]).as_ref());
-            lookup_parts.push('.');
-
-            i += length + 1;
-        }
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let resolver;
+    if args.len() > 1 {
+        resolver = args[2].to_owned();
+    } else {
+        resolver = "8.8.8.8:53".to_owned();
     }
 
-    lookups
-}
-
-fn main() {
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
+    let udp_send_socket = UdpSocket::bind("0.0.0.0:2054").expect("Failed to bind to address");
 
     // DNS is limited to 512 bytes
     let mut buf = [0; 512];
 
     loop {
         match udp_socket.recv_from(&mut buf) {
-            Ok((_, source)) => {
-                match Message::from_bytes((&buf, 0)) {
+            Ok((num_bytes_sent, source)) => {
+                let bytes_sent = &buf[0..num_bytes_sent];
+
+                match Header::from_bytes((&bytes_sent, 0)) {
                     Ok(result) => {
-                        let mut message = result.1;
+                        let (remaining_bytes, _) = result.0;
+                        let header = result.1;
+                        let mut answers = Vec::new();
 
-                        message.header.qr = 1;
-                        message.header.ancount = 2;
-                        message.header.rcode = if message.header.opcode == 0 { 0 } else { 4 };
+                        let questions_result = Question::read_questions(
+                            remaining_bytes,
+                            header.qdcount,
+                            bytes_sent.len() - remaining_bytes.len(),
+                        );
+                        let questions = questions_result.1;
 
+                        for q in &questions {
+                            let mut cloned_header = header.clone();
+
+                            cloned_header.qdcount = 1;
+                            cloned_header.arcount = 0;
+                            cloned_header.nscount = 0;
+                            cloned_header.qr = 0;
+                            let message = Message {
+                                header: cloned_header,
+                                question: vec![q.clone()],
+                                answer: vec![],
+                            };
+
+                            let mut bytes = Vec::with_capacity(512);
+                            message.to_bytes(&mut bytes).expect("");
+
+                            let to_bytes = bytes.as_bytes();
+                            match udp_send_socket.send_to(&to_bytes, &resolver) {
+                                Ok(_) => {
+                                    let mut forward = [0; 512];
+                                    let udp_rcv_result = udp_send_socket
+                                        .recv_from(&mut forward)
+                                        .expect("Failed to receive");
+
+                                    let header_result =
+                                        Header::from_bytes((&forward[0..udp_rcv_result.0], 0))
+                                            .expect("");
+
+                                    let (remaining_bytes, _) = header_result.0;
+                                    let header = header_result.1;
+
+                                    let questions_result = Question::read_questions(
+                                        remaining_bytes,
+                                        header.qdcount,
+                                        udp_rcv_result.0 - remaining_bytes.len(),
+                                    );
+                                    let (remaining_question_bytes, _) = questions_result.0;
+
+                                    let answers_result = Answer::read_answers(
+                                        remaining_question_bytes,
+                                        header.qdcount,
+                                        questions_result.1,
+                                    );
+
+                                    answers.extend(answers_result.1);
+                                }
+                                Err(e) => {
+                                    eprintln!("Error: {}", e);
+                                    panic!();
+                                }
+                            }
+                        }
+
+                        let mut cloned_return_header = header.clone();
+                        cloned_return_header.qr = 1;
+                        cloned_return_header.ancount = answers.len() as u16;
+                        cloned_return_header.qdcount = questions.len() as u16;
+                        cloned_return_header.rcode = if cloned_return_header.opcode == 0 {
+                            0
+                        } else {
+                            4
+                        };
+                        let return_msg = Message {
+                            header: cloned_return_header,
+                            question: questions,
+                            answer: answers,
+                        };
+
+                        let mut return_bytes = Vec::with_capacity(512);
+                        return_msg.to_bytes(&mut return_bytes).expect("");
                         udp_socket
-                            .send_to(message.to_bytes().unwrap().as_bytes(), source)
+                            .send_to(return_bytes.as_bytes(), source)
                             .expect("Failed to send response");
                     }
-                    Err(_) => {
-                        continue;
-                    }
+                    Err(_) => {}
                 };
             }
             Err(e) => {
